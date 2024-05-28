d3d4da652ab6b2a430356321d017a60c92f53641
Diogo Caxiado <caxiadodev@gmail.com>
2024-05-28 20:03:21 -0300

    test

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 
 client.once('ready', () => {
-    console.log('Bot do Discord está online!');
+    console.log('Bot Nara está online!');
 });
 
 // COMANDOS
